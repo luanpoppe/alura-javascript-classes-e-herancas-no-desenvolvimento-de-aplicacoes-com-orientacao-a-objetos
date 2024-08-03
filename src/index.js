f60a5495e1dcbffbe926c57491f0057c2c47d518
luanpoppe <luanpoppe@gmail.com>
2024-08-03 14:08:40 -0300

    feat: finalizado a seção 01 do curso

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-const personagemPedrinho = {
-    nome: 'Pedrinho',
-    vida: 7,
-    mana: 12,
-    level: 5,
-    tipo: 'Mago',
-}
+import { Personagem } from "./modules/personagem.js";
 
-const personagemJose = {
-    nome: 'Jose',
-    vida: 7,
-    mana: 6,
-    level: 3,
-    tipo: 'Arqueiro',
-}
+const personagemPedrinho = new Personagem();
+personagemPedrinho.name = "Pedrinho";
+personagemPedrinho.mana = 12;
+personagemPedrinho.vida = 7;
+personagemPedrinho.tipo = "Mago";
+personagemPedrinho.level = "5";
 
+console.log("personagemPedrinho", personagemPedrinho);
+
+const personagemJose = new Personagem();
+personagemJose.nome = "José";
+personagemJose.tipo = "Arqueiro";
+personagemJose.level = 3;
